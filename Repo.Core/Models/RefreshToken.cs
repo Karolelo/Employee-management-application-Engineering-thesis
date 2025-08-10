5063c0e4881cd21fe6720ec2fca97b40b652fb1c
MroziuOfficial <kubamaciekm22@gmail.com>
2025-08-10 21:50:15 +0200

    fix: added RefreshToken.cs

---
+++ Repo.Core/Models/RefreshToken.cs
@@ -0,0 +1,18 @@
+﻿namespace Repo.Core.Models;
+
+public partial class RefreshToken
+{
+    public int ID { get; set; }
+
+    public int User_ID { get; set; }
+
+    public string Token { get; set; } = null!;
+
+    public DateTime ExpireDate { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime? RevokedAt { get; set; }
+
+    public virtual User User { get; set; } = null!;
+}
