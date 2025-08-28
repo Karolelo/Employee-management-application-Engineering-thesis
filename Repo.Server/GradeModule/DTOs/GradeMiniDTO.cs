f5796459263c027c0e5c7dede10a6877f46a8fe4
MroziuOfficial <kubamaciekm22@gmail.com>
2025-08-28 18:07:17 +0200

    feat(grade): add GradeDTO and GradeMiniDTO
    - include Description, Start_Date, Finish_Date in both DTOs

---
+++ Repo.Server/GradeModule/DTOs/GradeMiniDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Repo.Server.GradeModule.DTOs;
+
+public class GradeMiniDTO
+{
+    [Required]
+    [Range(0, 100)]
+    public decimal Grade { get; set; }
+    
+    [Required]
+    public string Description { get; set; } = null!;
+    
+    [Required]
+    public DateOnly Start_Date { get; set; }
+    
+    [Required]
+    public DateOnly Finish_Date { get; set; }
+}
