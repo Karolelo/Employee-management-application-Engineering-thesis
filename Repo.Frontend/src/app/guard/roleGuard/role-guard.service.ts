11c7868078dde8ac757c665712568a7bd63f6f1b
Karol <kamasahi192@gmail.com>
2025-10-20 17:43:43 +0200

    feat(users): add user management card with filtering
    - add user management UI component
    - improve user list filtering logic
    - refactor data source for better performance
    - fix case-sensitive path issues

--- Repo.Frontend/src/app/guard/roleGuard/role-guard.service.ts
+++ Repo.Frontend/src/app/guard/roleGuard/role-guard.service.ts
@@ -10,13 +10,12 @@ import {UserStoreService} from '../../modules/login/services/user_data/user-stor
   providedIn: 'root'
 })
 export class RoleGuardService implements CanActivate {
-  constructor(private auth: AuthService, private router: Router,private userStore: UserStoreService) {}
+  constructor( private router: Router,private userStore: UserStoreService) {}
   canActivate(route: ActivatedRouteSnapshot): boolean {
     // this will be passed from the route config
     // on the data property
     const expectedRole = route.data['expectedRole'];
     if (
-      !this.auth.isAuthenticated() ||
       !this.userStore.hasRole(expectedRole)
     ) {
       this.router.navigate(['/login']);
--- Repo.Frontend/src/app/modules/dashboard/components/admin-dashboard/admin-dashboard.component.ts
+++ Repo.Frontend/src/app/modules/dashboard/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,36 +1,49 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject,OnInit } from '@angular/core';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 import { map } from 'rxjs/operators';
-
+import {UserService} from '../../services/user.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.css',
   standalone: false
 })
-export class AdminDashboardComponent {
+export class AdminDashboardComponent implements OnInit{
   private breakpointObserver = inject(BreakpointObserver);
-
+  private userService: UserService = inject(UserService);
+  public userCount = 0;
+  private router: Router = inject(Router);
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe(
+      (data) => {
+        this.userCount = data.length;
+      })
+  }
   /** Based on the screen size, switch from standard to one column per row */
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
       if (matches) {
         return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
+          { title: 'Card 1', cols: 1, rows: 1,type: 'userStats' },
+          { title: 'Card 2', cols: 1, rows: 1,type: 'groupsStats' },
+          { title: 'Card 3', cols: 1, rows: 1,type: 'tasksStats' },
+          { title: 'Card 4', cols: 1, rows: 1,type: 'other' }
         ];
       }
 
       return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
+        { title: 'Users Managment', cols: 2, rows: 1, type: 'userStats' },
+        { title: 'Card 2', cols: 1, rows: 1, type: 'groupsStats' },
+        { title: 'Card 3', cols: 1, rows: 2, type: 'tasksStats'},
+        { title: 'Card 4', cols: 1, rows: 1, type: 'other' }
       ];
     })
   );
+
+  onManageUserClick() {
+    this.router.navigate(['dashboard/users']);
+  }
 }
 //Fajny sposób na dodanie menu w math components
 /*
