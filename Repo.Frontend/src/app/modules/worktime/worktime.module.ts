5c26ec4a00384fa21d990fa3b50adb9d7bd220d4
MroziuOfficial <kubamaciekm22@gmail.com>
2025-11-13 19:21:53 +0100

    feat(worktime): added work entry form component - Added a form for managing the creation and edition of work entries - Updated declarations in module file

--- Repo.Frontend/src/app/modules/worktime/worktime.module.ts
+++ Repo.Frontend/src/app/modules/worktime/worktime.module.ts
@@ -3,10 +3,13 @@ import { CommonModule, NgIf, NgForOf, NgClass } from '@angular/common';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { WorktimeRoutingModule } from './worktime-routing.module';
 
+import { WorkEntryFormComponent } from './components/work-entry-form/work-entry-form.component';
+
 import { MatProgressSpinner } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
+    WorkEntryFormComponent,
   ],
   imports: [
     CommonModule,
