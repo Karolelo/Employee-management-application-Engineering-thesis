199a8e182a73d70b04857ab4e289a3d21a9d463d
MroziuOfficial <kubamaciekm22@gmail.com>
2025-11-14 16:22:40 +0100

    feat(worktime): added work entry list component - Added a list that displays work entries - Admin can filter work entries for a specified user - User can only see his work entries - Updated declarations in module file

--- Repo.Frontend/src/app/modules/worktime/worktime.module.ts
+++ Repo.Frontend/src/app/modules/worktime/worktime.module.ts
@@ -4,12 +4,14 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { WorktimeRoutingModule } from './worktime-routing.module';
 
 import { WorkEntryFormComponent } from './components/work-entry-form/work-entry-form.component';
+import { WorkEntryListComponent } from './components/work-entry-list/work-entry-list.component';
 
 import { MatProgressSpinner } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
     WorkEntryFormComponent,
+    WorkEntryListComponent,
   ],
   imports: [
     CommonModule,
