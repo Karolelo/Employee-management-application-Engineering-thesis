db693dd4802bd4821d2cb9904963da8955b65f31
MroziuOfficial <kubamaciekm22@gmail.com>
2026-01-18 19:54:29 +0100

    feat(profile): added DTOs for profile module

---
+++ Repo.Core/Models/profile/ChangeEmailDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace Repo.Core.Models.profile;
+
+public class ChangeEmailDTO
+{
+    public string CurrentPassword { get; set; } = null!;
+    public string NewEmail { get; set; } = null!;
+}
